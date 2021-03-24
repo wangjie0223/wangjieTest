aeefc220740a2c69028ecf4f8a8675a7088687ab
jqrios <wangjie@jqrjl.com>
2021-03-24 16:08:58 +0800

    my test first commit

---
+++ Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  StudyFramework
+//
+//  Created by jqrios on 2021/3/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+- (void)printLog;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  StudyFramework
+//
+//  Created by jqrios on 2021/3/19.
+//
+
+#import "Test.h"
+
+@implementation Test
+- (void)printLog{
+    NSLog(@"66666666");
+}
+
+@end
