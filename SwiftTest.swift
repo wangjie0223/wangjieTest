2786da798eccfb8893b55943cde64a23ee733978
jqrios <wangjie@jqrjl.com>
2021-03-25 13:59:50 +0800

    添加SwiftTest

---
+++ SwiftTest.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftTest.swift
+//  StudyFramework
+//
+//  Created by jqrios on 2021/3/19.
+//
+
+import UIKit
+
+class SwiftTest: NSObject {
+
+    override init() {
+        print("Swift^^^^666")
+    }
+}
